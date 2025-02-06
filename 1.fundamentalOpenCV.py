ffc7a7d332a2b936896aedc4a87d0b04415a94df
sugitou <sugi_tou108@yahoo.co.jp>
2025-02-06 22:27:33 +0000

    First commit

---
+++ 1.fundamentalOpenCV.py
@@ -0,0 +1,17 @@
+# %%
+import cv2
+import numpy as np
+import matplotlib
+import scipy
+import mahotas
+import skimage
+import sklearn
+
+print(cv2.__version__)
+print(np.__version__)
+print(matplotlib.__version__)
+print(scipy.__version__)
+print(mahotas.__version__)
+print(skimage.__version__)
+print(sklearn.__version__)
+# %%
